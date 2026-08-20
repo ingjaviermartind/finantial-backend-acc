5cc4b28035b175bbfac557f23703fc34d488d29f
Javier Martín <ingjavier.martind@gmail.com>
2026-08-20 18:11:00 -0500

    Se modifico el query de servicios activos para traer el MRC activo

--- Backend/sql/ser_queries.py
+++ Backend/sql/ser_queries.py
@@ -1,71 +1,94 @@
 
 QUERY_ACTIVE_SERVICES = """
-    WITH Servicios AS
+    WITH Servicios AS 
     (
-        SELECT *,
-            CASE
-                WHEN DIGITO_VERIFICACION IS NULL
-                    THEN CAST(NRO_IDENTIFICACION AS varchar(20))
-                ELSE CONCAT(NRO_IDENTIFICACION, '-', DIGITO_VERIFICACION)
-            END AS NIT
-        FROM DTM.SF_SERVICE_LEGV2
+        SELECT *, 
+            CASE 
+                WHEN DIGITO_VERIFICACION IS NULL 
+                    THEN CAST(NRO_IDENTIFICACION AS varchar(20)) 
+                ELSE CONCAT(NRO_IDENTIFICACION, '-', DIGITO_VERIFICACION) 
+            END AS NIT 
+        FROM DTM.SF_SERVICE_LEGV2 
+    ),
+
+    ServiciosActivos AS
+    (
+        SELECT *
+        FROM Servicios s
+        WHERE s.ESTADO_SER NOT IN ( 
+            'Cancelado', 
+            'Error', 
+            'En Proceso', 
+            'Declinado' 
+        )
+        AND s.NIT NOT IN ( 
+            '800136835-1',
+            '800153993-7',
+            '800255754-1',
+            '806009543-2',
+            '811021654-9',
+            '819006966-8',
+            '830053800-4',
+            '830058677-7',
+            '830078515-8',
+            '830114921-1',
+            '830122566-1',
+            '890905065-2',
+            '899999115-8',
+            '900092385-9',
+            '900195679-1',
+            '900258177-8',
+            '901354361-1'
+        )
+        AND s.TARIFA > 1
+    ),
+
+    TarifaCliente AS
+    (
+        SELECT
+            NIT,
+            SUM(TARIFA) AS TARIFA_TOTAL_CLIENTE
+        FROM ServiciosActivos
+        GROUP BY NIT
     )
 
-    SELECT 
-        s.NIT,
-        s.RAZON_SOCIAL AS [Razón Social],
-        s.ANCHODEBANDA AS [Capacidad],
-        s.CAPACIDADBPS,
-        s.TARIFA AS [Tarifa],
-        s.TARIFA / s.CAPACIDADBPS AS [Vlr x Mbps],
-        s.FECHA_FIN_PERMANENCIA AS [Fecha Fin Permanencia]
-    FROM Servicios s
-    WHERE s.ESTADO_SER NOT IN (
-        'Cancelado',
-        'Error',
-        'En Proceso',
-        'Declinado'
-    ) AND s.[PLAN] IN (
-        'CANAL NACIONAL ETHERNET',
-        'IRU DE CAPACIDAD',
-        'CANAL NACIONAL ETHERNET SIN UK',
-        'ID CORPORATIVO',
-        'INTERNET DEDICADO SIN UK',
-        'INTERNET DEDICADO SIN UK BURST',
-        'INTERNET DEDICADO EMPRESARIAL',
-        'RED IP',
-        'TRELUS INTERNET DEDICADO',
-        'INTERNET SIMETRICO EMPRESARIAL',
-        'BA CORPORATIVA',
-        'INTERNET + ALTO VALOR ESTRATO(1-3)',
-        'INTERNET + ALTO VALOR ESTRATO(4-6)',
-        'INTERNET +'
-    )
-    --AND SegmentacionIVR = 'ISPs'
-    AND s.NIT NOT IN (
-        '800136835-1', -- cirion
-        '800153993-7', -- claro
-        '800255754-1', -- sencinet latam colombia sa
-        '806009543-2', -- ufinet colombia sa
-        '811021654-9', -- internexa
-        '819006966-8', -- media commerce
-        '830053800-4', -- telmex
-        '830058677-7', -- ifx
-        '830078515-8', -- liberty networks 
-        '830114921-1', -- tigo
-        '830122566-1', -- telefonica
-        '890905065-2', -- edatel
-        '899999115-8', -- etb
-        '900092385-9', -- une
-        '900195679-1', -- gtd colombia sas
-        '900258177-8', -- Globenet 
-        '901354361-1' -- partners telecom colombia sas
-    )
-    AND s.TARIFA > 1
-    AND SER NOT IN  ('SER-280627')
-    AND s.CAPACIDADBPS >= :min_cap
-    AND s.[Codigo DANE] = :dane
-    ORDER BY s.CAPACIDADBPS DESC
+    SELECT  
+        s.NIT, 
+        s.RAZON_SOCIAL AS [Razón Social], 
+        s.ANCHODEBANDA AS [Capacidad], 
+        s.CAPACIDADBPS, 
+        s.TARIFA AS [Tarifa], 
+        s.TARIFA / s.CAPACIDADBPS AS [Vlr x Mbps], 
+        s.FECHA_FIN_PERMANENCIA AS [Fecha Fin Permanencia],
+        tc.TARIFA_TOTAL_CLIENTE AS [Tarifa Total Cliente]
+
+    FROM ServiciosActivos s
+
+    LEFT JOIN TarifaCliente tc
+        ON s.NIT = tc.NIT
+
+    WHERE
+        s.SER NOT IN ('SER-280627') 
+        AND s.[PLAN] IN (
+            'CANAL NACIONAL ETHERNET',
+            'IRU DE CAPACIDAD',
+            'CANAL NACIONAL ETHERNET SIN UK',
+            'ID CORPORATIVO',
+            'INTERNET DEDICADO SIN UK',
+            'INTERNET DEDICADO SIN UK BURST',
+            'INTERNET DEDICADO EMPRESARIAL',
+            'RED IP',
+            'TRELUS INTERNET DEDICADO',
+            'INTERNET SIMETRICO EMPRESARIAL',
+            'BA CORPORATIVA',
+            'INTERNET + ALTO VALOR ESTRATO(1-3)',
+            'INTERNET + ALTO VALOR ESTRATO(4-6)',
+            'INTERNET +'
+        )
+        AND s.CAPACIDADBPS >= :min_cap
+        AND s.[Codigo DANE] = :dane 
+
+    ORDER BY s.CAPACIDADBPS DESC;
         """
 
 QUERY_SERVICES_REFERENCE_MUN = """
